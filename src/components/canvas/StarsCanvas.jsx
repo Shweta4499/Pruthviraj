d48c408e501039c5b142d45f4dd73c1b022cc465
Shweta Gurav <shwetagurav4499@gmail.com>
2025-08-01 18:52:02 +0530

    Star bg

--- src/components/canvas/StarsCanvas.jsx
+++ src/components/canvas/StarsCanvas.jsx
@@ -1,22 +1,46 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { Preload, Stars } from "@react-three/drei";
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
 
+
+const Stars = (props) => {
+  const ref = useRef();
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color='#f272c8'
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
 const StarsCanvas = () => {
   return (
-    
-    <Canvas
-      frameloop="demand"
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-      camera={{ position: [0, 0, 1] }}
-    >
-      <Suspense fallback={null}>
-        <Stars radius={80} depth={50} count={5000} factor={4} saturation={0} fade speed={0.8} />
+    <div className='w-full h-auto absolute inset-0 z-[-1]'>
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
         <Preload all />
-      </Suspense>
-    </Canvas>
+      </Canvas>
+    </div>
   );
 };
 
-export default StarsCanvas; 
+export default StarsCanvas;
